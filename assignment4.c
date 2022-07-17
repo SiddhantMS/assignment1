8954323c91568964e30b4fba84547790e9b70da6
sidhant <90951358+SiddhantMS@users.noreply.github.com>
2022-07-18 00:50:49 +0530

    Add files via upload

---
+++ assignment4.c
@@ -0,0 +1,84 @@
+
+#include <stdio.h>
+
+int main()
+{
+    printf("1.\n");
+    for(int i=0;i<=4;i++){
+        printf("My SirG\n");
+    }
+    printf("\n\n");
+    printf("2.\n");
+       for(int i=1;i<=10;i++){
+        printf("%d\n",i);
+    }
+        
+     printf("\n\n");
+    printf("3.\n");    
+     for(int i=10;i>0;i--){
+        printf("%d\n",i);
+    }
+    
+    printf("\n\n");
+    printf("4.\n");
+    int no=1;
+    printf("%d\n",no);
+    for(int i=0;i<=10;i++){
+        no=no+2;
+        printf("%d\n",no);
+    }
+    
+     printf("\n\n");
+    printf("5.\n");
+    int noo=23;
+    printf("%d\n",no);
+    for(int i=0;i<=10;i++){
+        noo=noo-2;
+        printf("%d\n",noo);
+    }
+    
+     printf("\n\n");
+    printf("6.\n");
+     int nio=2;
+    printf("%d\n",nio);
+    for(int i=0;i<=10;i++){
+        nio=nio+2;
+        printf("%d\n",nio);
+}
+     printf("\n\n");
+    printf("7.\n");
+    int noio=24;
+    printf("%d\n",noio);
+    for(int i=0;i<=10;i++){
+        noio=noio-2;
+        printf("%d\n",noio);
+    }
+    
+      printf("\n\n");
+    printf("8.\n");
+       for(int i=1;i<=10;i++){
+           int sqr;
+           sqr=i*i;
+        printf("the sqr of %d is %d\n",i,sqr);
+    }
+    
+      printf("\n\n");
+    printf("9.\n");
+       for(int i=1;i<=10;i++){
+           int cube;
+           cube=i*i*i;
+        printf("the cube of %d is %d\n",i,cube);
+    }
+    
+     printf("\n\n");
+    printf("10.\n table of 5\n");
+       for(int i=1;i<=10;i++){
+           int table;
+           table=5*i;
+        printf(" 5 * %d = %d\n",i,table);
+    }
+    
+}
+
+
+    
